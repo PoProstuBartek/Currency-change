43c7d411b9c549928b93db1656f81e1e2518f2f5
Bartłomiej Szczotkowski <szczotkowski.b@gmail.com>
2022-01-30 13:49:58 +0100

    First automated tests

--- src/utils/convertPLNToUSD.js
+++ src/utils/convertPLNToUSD.js
@@ -1,4 +1,29 @@
 export const convertPLNToUSD = (PLN) => {
+
+  if(typeof PLN === 'string'){
+    return NaN;
+  }
+
+  if (PLN < 0 ){
+    return '$0.00';
+  }
+
+  if (typeof PLN === 'object'){
+    return 'Error';
+  }
+
+  if (PLN === null){
+    return 'Error';
+  }
+
+  if (typeof PLN === 'array'){
+    return 'Error';
+  }
+
+  if (typeof PLN === 'function'){
+    return 'Error';
+  }
+
   const PLNtoUSD = PLN / 3.5;
   const formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
